d72d105e566025a0e1cce45e6b18e94780de8475
鈴木一平 <ippei@suzukiippei-no-MacBook-Pro.local>
2016-03-21 10:40:18 +0900

    ページ計算検討中

--- routes/index.js
+++ routes/index.js
@@ -14,134 +14,109 @@ var packageJson = require('../package.json');
 
 /** メモ一覧を表示する。 */
 exports.list = function(req, res) {
-	// http://docs.couchdb.org/en/1.6.1/couchapp/views/pagination.html
-	var page = req.query.page;
+  // http://docs.couchdb.org/en/1.6.1/couchapp/views/pagination.html
+  var page = req.query.page;
 
-	// ビューのパラメータを設定する。
-	var viewParams = {
-		descending : true,
-		limit : context.LINE_PER_PAGE + 1
-	};
+  // ビューのパラメータを設定する。
+  var viewParams = {
+    descending: true,
+    limit: context.LINE_PER_PAGE + 1
+  };
 
-	// URLパラメータによりビューの設定を追加する。
-	var pageIndexs = req.query.pageIndexs;
-	if (typeof pageIndexs === 'undefined') {
-		// 最初のページ (初回の検索)
-		// ページインデックスを初期化する。(0, 1ページは未使用)
-		pageIndexs = [ undefined, undefined ];
-		page = 1;
-	} else {
-		pageIndexs = pageIndexs.split(',');
-		// ページインデックスから startkey を設定する。値が無い場合は
-		if (typeof pageIndexs[page] === 'undefined') {
-			page = 1;
-		} else {
-			viewParams.startkey = pageIndexs[page] !== '' ? pageIndexs[page]
-					: undefined;
-		}
-	}
+  // URLパラメータによりビューの設定を追加する。
+  var pageIndexs = req.query.pageIndexs;
+  if (page == 1 || typeof pageIndexs === 'undefined') {
+    // 最初のページ (初回の検索)
+    // ページインデックスを初期化する。(0, 1ページは未使用)
+    pageIndexs = [undefined, undefined];
+    page = 1;
+  } else {
+    pageIndexs = pageIndexs.split(',');
+    // ページインデックスから startkey を設定する。値が無い場合は
+    if (typeof pageIndexs[page] === 'undefined') {
+      page = 1;
+    } else {
+      viewParams.startkey = pageIndexs[page];
+    }
+  }
 
-	console.log(pageIndexs);
+  console.log(pageIndexs);
+  console.log(viewParams.startkey);
 
-	console.log(viewParams.startkey);
+  memo.list(viewParams, function(err, body) {
+    if (body.rows.length > context.LINE_PER_PAGE) {
+      var end = body.rows.pop();
+      pageIndexs[parseInt(page) + 1] = end.key;
+    }
 
-	memo.list(viewParams, function(err, body) {
-		if (body.rows.length > context.LINE_PER_PAGE) {
-			var end = body.rows.pop();
-			pageIndexs[parseInt(page) + 1] = end.key;
-		}
-/*
+    // 前のページ
+    var previousPage;
+    if (page > 1) {
+      previousPage = parseInt(page) - 1;
+    }
 
+    // 次のページ
+    var nextPage;
+    if (page < Math.floor(body.total_rows / context.LINE_PER_PAGE) + 1) {
+      nextPage = parseInt(page) + 1;
+    }
 
-		page == 1 : 前なし
-
-		page => Math.floor(body.total_rows / context.LINE_PER_PAGE) + 1 : 次無し
-
-
-		previous =
-
-		next =
-
-*/
-		res.render('index', {
-			"packageJson" : packageJson,
-			"list" : body.rows,
-			"page" : page,
-			"pageIndexs" : pageIndexs,
-		});
-	});
+    res.render('index', {
+      "packageJson": packageJson,
+      "list": body.rows,
+      "page": page,
+      "pageIndexs": pageIndexs,
+      "previousPage": previousPage,
+      "nextPage": nextPage
+    });
+  });
 };
 
-/*var previous = function(page){
-	if(page<=1){
-
-	}else{
-
-	}
-
-
-
-};
-
-
-pagingInfo= {
-	"page"
-	"previous":{
-		"visible": true,
-		"page": ""
-	},
-	"next":{
-		"visible": true,
-		page: ""
-	}
-};
-
-*/
 
 /** メモの新規作成ダイアログを表示する。 */
 exports.createDialog = function(req, res) {
-	res.render('dialog', {
-		"doc" : null
-	});
+  res.render('dialog', {
+    "doc": null
+  });
 };
 
 /** メモの更新ダイアログを表示する。 */
 exports.updateDialog = function(req, res) {
-	var _id = req.params._id;
-	memo.get(_id, function(err, doc) {
-		res.render('dialog', {
-			"doc" : doc
-		});
-	});
+  var _id = req.params._id;
+  memo.get(_id, function(err, doc) {
+    res.render('dialog', {
+      "doc": doc
+    });
+  });
 };
 
 /** メモをDBに新規作成して、メモ一覧を表示する。 */
 exports.create = function(req, res) {
-	var doc = {
-		"content" : req.body.content,
-		"updatedAt" : req.body.updatedAt
-	};
-	memo.save(doc, function() {
-		res.redirect('/');
-	});
+  var doc = {
+    "content": req.body.content,
+    "updatedAt": req.body.updatedAt
+  };
+  memo.save(doc, function() {
+    res.redirect('/');
+  });
 };
 
 /** メモをDBに更新して、メモ一覧を表示する。 */
 exports.update = function(req, res) {
-	var doc = {
-		"_id" : req.params._id,
-		"_rev" : req.params._rev,
-		"content" : req.body.content,
-		"updatedAt" : req.body.updatedAt
-	};
-	memo.save(doc, function() {
-		res.redirect('/');
-	});
+  var doc = {
+    "_id": req.params._id,
+    "_rev": req.params._rev,
+    "content": req.body.content,
+    "updatedAt": req.body.updatedAt
+  };
+  memo.save(doc, function() {
+    res.redirect('/');
+  });
 };
 
 /** メモをDBから削除して、メモ一覧を表示する。 */
 exports.remove = function(req, res) {
-	memo.remove(req.params._id, req.params._rev, function() {
-		res.redirect('/');
-	});
+  memo.remove(req.params._id, req.params._rev, function() {
+    res.redirect('/');
+  });
 };
